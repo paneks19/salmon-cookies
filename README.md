d9508f127e13659501e697701c1e66ebc98e7b6b
Simon Panek <paneks19@gmail.com>
2020-10-05 15:14:59 -0700

    first object created

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# salmon-cookies
-201-lab-06
+# LAB - 201:06
+
+## Salmon Cookies
+
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+### Author: Simon Panek
+
+### Links and Resources
+
+* 
+
+### Reflections and Comments
+
+* 
+
+### Data provided
+
+- Hours = 6am to 8pm
+
+Location | Min/Cust | Max/Cust | Avg Cookie/Sale
+---|---|---|---
+Seattle | 23 | 65 | 6.3
+Tokyo | 3 | 24 | 1.2
+Dubai | 11 | 38 | 2.3
+Paris | 20 | 38 | 2.3
+Lima | 2 | 16 | 4.6
+
--- js/app.js
+++ js/app.js
@@ -1,2 +1,80 @@
 'use strict';
-alert('JS connected');
+// alert('JS connected');
+
+//need an object for each of the locations: Seattle, Tokyo, Dubai, Paris, Lima
+
+var seattleObject = {
+    minCustomers: 23,       //minimum number of customers per hour
+    maxCustomers: 65,       //maximum number of customers per hour
+    avgCookie: 6.3,         //average cooke sales per hour
+
+    customersPerHour: [], //array to store the number of customers per hour
+    cookieSalesPerHour: [], //array to store the calculated number of cookies sold per hour
+}
+
+
+
+var tokyoObject = {
+    minCustomers: 23,       //minimum number of customers per hour
+    maxCustomers: 65,       //maximum number of customers per hour
+    avgCookie: 6.3,         //average cooke sales per hour
+    
+    customersPerHour: [], //array to store the number of customers per hour
+    cookieSalesPerHour: [], //array to store the calculated number of cookies sold per hour
+    
+}
+
+var dubaiObject = {
+    minCustomers: 23,       //minimum number of customers per hour
+    maxCustomers: 65,       //maximum number of customers per hour
+    avgCookie: 6.3,         //average cooke sales per hour
+    
+    customersPerHour: [], //array to store the number of customers per hour
+    cookieSalesPerHour: [], //array to store the calculated number of cookies sold per hour
+    
+}
+
+var parisObject = {
+    minCustomers: 23,       //minimum number of customers per hour
+    maxCustomers: 65,       //maximum number of customers per hour
+    avgCookie: 6.3,         //average cooke sales per hour
+    
+    customersPerHour: [], //array to store the number of customers per hour
+    cookieSalesPerHour: [], //array to store the calculated number of cookies sold per hour
+    
+}
+
+var limaObject = {
+    minCustomers: 23,       //minimum number of customers per hour
+    maxCustomers: 65,       //maximum number of customers per hour
+    avgCookie: 6.3,         //average cooke sales per hour
+    
+    customersPerHour: [], //array to store the number of customers per hour
+    cookieSalesPerHour: [], //array to store the calculated number of cookies sold per hour
+    
+}
+
+
+function hourSales(minCustomers, maxCustomers){ //generating a random number of customers per hour
+  var min = Math.ceil(minCustomers);
+  var max = Math.floor(maxCustomers);
+        return Math.floor(Math.random() * (max-min +1) + min);
+    }
+
+ function loopCalculator(object){ //loop through each hour of the 14 hour work day to calculate random sales for each hour
+   for (var i=0; i<14; i++){
+
+    var randomSale = hourSales(object.minCustomers, object.maxCustomers)
+
+    object.customersPerHour.push(randomSale); //calculates the random visits at each hour and appends to end of customersPerHour array
+
+     object.cookieSalesPerHour.push(object.avgCookie * randomSale); //multiplies avgCookie sales per 
+
+     console.log('customer per hour ', object.customersPerHour, 'cookieSalesPerHour ', object.cookieSalesPerHour); //print variable values to check
+    }
+    return object; //returns function data for use elsewhere
+
+}
+
+loopCalculator(seattleObject);
+// seattleObject.hourlyCalculator(seattleObject.minCustomers, seattleObject.maxCustomers);
